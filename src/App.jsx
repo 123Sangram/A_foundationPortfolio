081eb60050c82b93d21e232e1abe5b0e40c9bbd4
123Sangram <sangramamvan@gmail.com>
2024-10-22 12:53:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+
+const siteProps = {
+  title: "Sangram Singh",
+  email: "alex@example.com",
+  gitHub: "microsoft",
+  instagram: "microsoft",
+  linkedIn: "satyanadella",
+  medium: "",
+  twitter: "microsoft",
+  youTube: "Code",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer
+        {...siteProps}
+        primaryColor={primaryColor}
+        secondaryColor={secondaryColor}
+      />
+    </div>
+  );
+};
+
+export default App;
